1f36aec2f3e69db7421ecd21b19920ef7c5ccfd0
Joaquim Costa <jscosta88@gmail.com>
2025-07-09 23:22:15 -0400

    fix: Use property for Detekt formatting plugin version for consistency

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -31,7 +31,7 @@ extra["detektVersion"] = "1.23.8"
 
 dependencies {
 
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.8")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${property("detektVersion")}")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
