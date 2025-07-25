1f5277d66a5cb0ec791c641f6e10c0e510d4ef3e
Joaquim Costa <jscosta88@gmail.com>
2025-07-25 13:07:05 -0400

    feat: add custom dark variant support in globals.css and remove darkMode configuration from tailwind.config.ts

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,7 +7,6 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/catalyst-ui/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: ["variant", "&:where(.dark, .dark *)"],
   plugins: [forms],
 };
 export default config;
