576fb76579dcb32050a3d8f6418ceca47fda741f
Joaquim Costa <jscosta88@gmail.com>
2025-10-14 15:31:35 -0400

    ✨ feat(banner): update Banner component styles and add title to content

--- frontend/src/components/ui/banner.tsx
+++ frontend/src/components/ui/banner.tsx
@@ -31,6 +31,7 @@ export default function Banner({
 
   const content = (
     <>
+      <span aria-hidden="true">🇨🇻 ⚽</span>{" "}
       <strong className="font-semibold">{title}</strong>
       <svg
         viewBox="0 0 2 2"
@@ -41,6 +42,7 @@ export default function Banner({
       </svg>
       {message}&nbsp;
       {linkUrl && <span aria-hidden="true">&rarr;</span>}
+      {" "}<span aria-hidden="true">✨</span>
     </>
   );
 
@@ -55,7 +57,7 @@ export default function Banner({
             clipPath:
               "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
           }}
-          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30 dark:opacity-40"
+          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#D90368] to-[#005A8D] opacity-50 dark:opacity-60"
         />
       </div>
       <div
@@ -67,7 +69,7 @@ export default function Banner({
             clipPath:
               "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
           }}
-          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30 dark:opacity-40"
+          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#D90368] to-[#005A8D] opacity-50 dark:opacity-60"
         />
       </div>
       <p className="text-sm/6 text-gray-900 dark:text-gray-100">
