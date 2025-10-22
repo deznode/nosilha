e15943c804e5a1c8d84f6758f69faaf31d5c35c7
Joaquim Costa <jscosta88@gmail.com>
2025-10-21 22:36:39 -0400

    fix script

--- backend/src/main/kotlin/com/nosilha/core/media/config/LocalStorageConfig.kt
+++ backend/src/main/kotlin/com/nosilha/core/media/config/LocalStorageConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @Configuration
-@Profile("local")
+@Profile("local || test")
 class LocalStorageConfig(
     @Value("\${gcp.serverUrl}")
     private val fakeGcsExternalUrl: String,
