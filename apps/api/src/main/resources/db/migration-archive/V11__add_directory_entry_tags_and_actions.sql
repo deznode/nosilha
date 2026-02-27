99e575bcd80d9cc93567247680ffd16605d192a6
Joaquim Costa <jscosta88@gmail.com>
2026-02-26 22:34:44 -0500

    Refactor database migration scripts: - Remove rollback script for V33 migration (evolve_media_to_gallery). - Delete V34 migration that drops curated_media table. - Remove V35 migration updating directory_submissions category constraint. - Eliminate V36 migration adding directory contact fields. - Remove V37 migration applying research corrections. - Delete V38 migration fixing Faja d'Agua slug. - Remove V39 migration setting image URL for Igreja Nossa Senhora do Monte. - Delete V3 migration creating users table. - Remove V40 migration unifying directory tables. - Delete V4 migration adding timestamps to directory_entries. - Remove V5 migration creating towns table. - Delete V6 migration seeding towns with corrected data. - Remove V7 migration creating spring modulith event publication table. - Delete V8 migration creating reactions table. - Remove V9 migration creating suggestions table.

--- apps/api/src/main/resources/db/migration-archive/V11__add_directory_entry_tags_and_actions.sql
+++
@@ -1,7 +0,0 @@
--- Add manual tag metadata and content action overrides to directory entries
-ALTER TABLE directory_entries
-    ADD COLUMN tags TEXT,
-    ADD COLUMN content_actions JSONB;
-
-COMMENT ON COLUMN directory_entries.tags IS 'Comma-separated list of manually curated tags used for related-content matching';
-COMMENT ON COLUMN directory_entries.content_actions IS 'JSON configuration overriding default ContentActionToolbar behavior';
--- apps/api/src/main/resources/db/migration-archive/V39__set_igreja_image_url.sql
+++
@@ -1,7 +0,0 @@
--- V39__set_igreja_image_url.sql
--- Set the imageUrl for Igreja Nossa Senhora do Monte (downloaded from Wikimedia Commons)
--- Image stored in category-organized folder: /images/directory/heritage/
-
-UPDATE directory_entries SET
-    image_url = '/images/directory/heritage/igreja-nossa-senhora-do-monte.jpg'
-WHERE slug = 'igreja-nossa-senhora-do-monte';
